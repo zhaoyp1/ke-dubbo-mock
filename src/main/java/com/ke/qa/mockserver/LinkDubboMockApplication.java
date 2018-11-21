f1c06fa0449f00fa83874ecd6ccb473069245235
zhaoyunping <zhaoyunping001@ke.com>
2018-11-21 12:18:31 +0800

    add package

--- src/main/java/com/ke/qa/mockserver/LinkDubboMockApplication.java
+++ src/main/java/com/ke/qa/mockserver/LinkDubboMockApplication.java
@@ -1,4 +1,4 @@
-package com.ke.qa.mockserver.linkdubbomock;
+package com.ke.qa.mockserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/ke/qa/mockserver/ke/dubbo/mock/LinkDubboMockApplicationTests.java
+++ src/test/java/com/ke/qa/mockserver/ke/dubbo/mock/LinkDubboMockApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ke.qa.mockserver.linkdubbomock;
+package com.ke.qa.mockserver.ke.dubbo.mock;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
