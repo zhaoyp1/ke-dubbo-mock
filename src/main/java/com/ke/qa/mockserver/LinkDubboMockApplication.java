f48e31c0a42a62178bc90b94908a38546702709e
zhaoyunping <zhaoyunping001@ke.com>
2018-11-21 12:25:54 +0800

    add spring.xml

--- src/main/java/com/ke/qa/mockserver/LinkDubboMockApplication.java
+++ src/main/java/com/ke/qa/mockserver/LinkDubboMockApplication.java
@@ -3,6 +3,8 @@ package com.ke.qa.mockserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//@ImportResource({"classpath:spring/spring.xml"}) 若存在spring.xml,可以使用此种方式注入
 @SpringBootApplication
 public class LinkDubboMockApplication {
 
