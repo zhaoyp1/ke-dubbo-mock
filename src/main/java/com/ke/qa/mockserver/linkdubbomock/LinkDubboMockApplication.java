ad9df70b8a73d3dd8c6ccc40e89827ce47dfa285
zhaoyunping <zhaoyunping001@ke.com>
2018-11-21 12:12:37 +0800

    init master

---
+++ src/main/java/com/ke/qa/mockserver/linkdubbomock/LinkDubboMockApplication.java
@@ -0,0 +1,12 @@
+package com.ke.qa.mockserver.linkdubbomock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkDubboMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkDubboMockApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ke/qa/mockserver/linkdubbomock/LinkDubboMockApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ke.qa.mockserver.linkdubbomock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LinkDubboMockApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
